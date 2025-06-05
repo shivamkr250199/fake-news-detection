87dbfe6e82b56dc217f43b9ff049aba2290ea607
shivamkr250199 <122144637+shivamkr250199@users.noreply.github.com>
2025-06-05 16:55:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -44,6 +44,6 @@ def home():
 
 if __name__ == '__main__':
     
-port = int(os.environ.get("PORT", 5000))  # fallback to 5000 for local testing
-app.run(host='0.0.0.0', port=port)
+    port = int(os.environ.get("PORT", 5000))  # fallback to 5000 for local testing
+    app.run(host='0.0.0.0', port=port)
 
